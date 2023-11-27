56229b91b4f3deb2a198aa02c22f0c78981f45cc
Schnitzel5 <tangphilip3@gmail.com>
2023-11-27 13:23:42 +0100

    fixed mongodb compatibility

--- src/test/java/at/spengergasse/efees/EfeesApplicationTests.java
+++ src/test/java/at/spengergasse/efees/EfeesApplicationTests.java
@@ -1,13 +1,25 @@
 package at.spengergasse.efees;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EfeesApplicationTests {
 
+	@BeforeAll
+	static void beforeAll() {
+		System.out.println("----------MONGO TEST----------");
+	}
+
 	@Test
 	void contextLoads() {
+	}
+
+	@AfterAll
+	static void afterAll() {
+		System.out.println("----------MONGO TEST----------");
 	}
 
 }
